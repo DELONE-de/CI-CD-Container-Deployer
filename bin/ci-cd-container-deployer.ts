265c291edb42350813ae93d33e1b7ee15c3762da
DELONE-de <ibiyemiaina27@gmail.com>
2026-03-29 23:15:25 +0000

    Initialize CDK stacks and fix module resolution

--- bin/ci-cd-container-deployer.ts
+++ bin/ci-cd-container-deployer.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { NetworkStack } from '../lib/network-stack';
+import { EksStack } from '../lib/eks-stack';
+import { EcrStack } from '../lib/ecr-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
+
+const networkStack = new NetworkStack(app, 'NetworkStack');
+const ecrStack = new EcrStack(app, 'EcrStack');
+const eksStack = new EksStack(app, 'EksStack', { env: networkStack.env });
+new PipelineStack(app, 'PipelineStack');
